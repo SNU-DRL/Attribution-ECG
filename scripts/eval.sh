d6804e61e82952bff68891601c57a652c9c639a5
Jimyeong Kim <wlaud1001@snu.ac.kr>
2022-09-28 18:47:15 +0900

    fix script file
    Signed-off-by: Jimyeong Kim <wlaud1001@snu.ac.kr>

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -12,106 +12,71 @@
 #     'feature_ablation',
 # ]
 
+results_dir='results_eval_2'
+
 (
 gpu=12
 method=saliency
-python evaluate_attributions.py --correct --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --correct --gpu $gpu --attr_method $method 
-python evaluate_attributions.py --prob_thres 0.9 --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --prob_thres 0.9 --gpu $gpu --attr_method $method 
-python evaluate_attributions.py --prob_thres 0.8 --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --prob_thres 0.8 --gpu $gpu --attr_method $method 
-
-
-method=integrated_gradients
-python evaluate_attributions.py --correct --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --correct --gpu $gpu --attr_method $method 
-python evaluate_attributions.py --prob_thres 0.9 --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --prob_thres 0.9 --gpu $gpu --attr_method $method 
-python evaluate_attributions.py --prob_thres 0.8 --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --prob_thres 0.8 --gpu $gpu --attr_method $method 
+python evaluate_attributions.py --results_path $results_dir --prob_thres 0.9 --gpu $gpu --attr_method $method --absolute 
+python evaluate_attributions.py --results_path $results_dir --prob_thres 0.9 --gpu $gpu --attr_method $method 
 ) &
 (
 gpu=11
-method=input_gradient
-python evaluate_attributions.py --correct --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --correct --gpu $gpu --attr_method $method 
-python evaluate_attributions.py --prob_thres 0.9 --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --prob_thres 0.9 --gpu $gpu --attr_method $method 
-python evaluate_attributions.py --prob_thres 0.8 --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --prob_thres 0.8 --gpu $gpu --attr_method $method 
-
-method=lrp
-python evaluate_attributions.py --correct --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --correct --gpu $gpu --attr_method $method 
-python evaluate_attributions.py --prob_thres 0.9 --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --prob_thres 0.9 --gpu $gpu --attr_method $method 
-python evaluate_attributions.py --prob_thres 0.8 --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --prob_thres 0.8 --gpu $gpu --attr_method $method 
+method=integrated_gradients
+python evaluate_attributions.py --results_path $results_dir --prob_thres 0.9 --gpu $gpu --attr_method $method --absolute 
+python evaluate_attributions.py --results_path $results_dir --prob_thres 0.9 --gpu $gpu --attr_method $method 
 ) &
 (
 gpu=10
-method=lime
-python evaluate_attributions.py --correct --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --correct --gpu $gpu --attr_method $method 
-python evaluate_attributions.py --prob_thres 0.9 --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --prob_thres 0.9 --gpu $gpu --attr_method $method 
-python evaluate_attributions.py --prob_thres 0.8 --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --prob_thres 0.8 --gpu $gpu --attr_method $method 
-
-method=kernel_shap
-python evaluate_attributions.py --correct --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --correct --gpu $gpu --attr_method $method 
-python evaluate_attributions.py --prob_thres 0.9 --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --prob_thres 0.9 --gpu $gpu --attr_method $method 
-python evaluate_attributions.py --prob_thres 0.8 --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --prob_thres 0.8 --gpu $gpu --attr_method $method 
+method=input_gradient
+python evaluate_attributions.py --results_path $results_dir --prob_thres 0.9 --gpu $gpu --attr_method $method --absolute 
+python evaluate_attributions.py --results_path $results_dir --prob_thres 0.9 --gpu $gpu --attr_method $method 
 ) &
 (
 gpu=9
-method=deep_lift
-python evaluate_attributions.py --correct --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --correct --gpu $gpu --attr_method $method 
-python evaluate_attributions.py --prob_thres 0.9 --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --prob_thres 0.9 --gpu $gpu --attr_method $method 
-python evaluate_attributions.py --prob_thres 0.8 --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --prob_thres 0.8 --gpu $gpu --attr_method $method 
-
-method=deep_lift_shap
-python evaluate_attributions.py --correct --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --correct --gpu $gpu --attr_method $method 
-python evaluate_attributions.py --prob_thres 0.9 --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --prob_thres 0.9 --gpu $gpu --attr_method $method 
-python evaluate_attributions.py --prob_thres 0.8 --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --prob_thres 0.8 --gpu $gpu --attr_method $method 
+method=lrp
+python evaluate_attributions.py --results_path $results_dir --prob_thres 0.9 --gpu $gpu --attr_method $method --absolute 
+python evaluate_attributions.py --results_path $results_dir --prob_thres 0.9 --gpu $gpu --attr_method $method 
 ) &
 (
 gpu=8
-method=gradcam
-python evaluate_attributions.py --correct --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --correct --gpu $gpu --attr_method $method 
-python evaluate_attributions.py --prob_thres 0.9 --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --prob_thres 0.9 --gpu $gpu --attr_method $method 
-python evaluate_attributions.py --prob_thres 0.8 --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --prob_thres 0.8 --gpu $gpu --attr_method $method 
+method=lime
+python evaluate_attributions.py --results_path $results_dir --prob_thres 0.9 --gpu $gpu --attr_method $method --absolute 
+python evaluate_attributions.py --results_path $results_dir --prob_thres 0.9 --gpu $gpu --attr_method $method 
 ) &
 (
 gpu=7
-method=guided_gradcam
-python evaluate_attributions.py --correct --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --correct --gpu $gpu --attr_method $method 
-python evaluate_attributions.py --prob_thres 0.9 --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --prob_thres 0.9 --gpu $gpu --attr_method $method 
-python evaluate_attributions.py --prob_thres 0.8 --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --prob_thres 0.8 --gpu $gpu --attr_method $method 
+method=kernel_shap
+python evaluate_attributions.py --results_path $results_dir --prob_thres 0.9 --gpu $gpu --attr_method $method --absolute 
+python evaluate_attributions.py --results_path $results_dir --prob_thres 0.9 --gpu $gpu --attr_method $method 
 ) &
 (
 gpu=6
+method=deep_lift
+python evaluate_attributions.py --results_path $results_dir --prob_thres 0.9 --gpu $gpu --attr_method $method --absolute 
+python evaluate_attributions.py --results_path $results_dir --prob_thres 0.9 --gpu $gpu --attr_method $method 
+) &
+(
+gpu=5
+method=deep_lift_shap
+python evaluate_attributions.py --results_path $results_dir --prob_thres 0.9 --gpu $gpu --attr_method $method --absolute 
+python evaluate_attributions.py --results_path $results_dir --prob_thres 0.9 --gpu $gpu --attr_method $method 
+) &
+(
+gpu=4
+method=gradcam
+python evaluate_attributions.py --results_path $results_dir --prob_thres 0.9 --gpu $gpu --attr_method $method --absolute 
+python evaluate_attributions.py --results_path $results_dir --prob_thres 0.9 --gpu $gpu --attr_method $method 
+) &
+(
+gpu=3
+method=guided_gradcam
+python evaluate_attributions.py --results_path $results_dir --prob_thres 0.9 --gpu $gpu --attr_method $method --absolute 
+python evaluate_attributions.py --results_path $results_dir --prob_thres 0.9 --gpu $gpu --attr_method $method 
+) &
+(
+gpu=2
 method=feature_ablation
-python evaluate_attributions.py --correct --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --correct --gpu $gpu --attr_method $method 
-python evaluate_attributions.py --prob_thres 0.9 --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --prob_thres 0.9 --gpu $gpu --attr_method $method 
-python evaluate_attributions.py --prob_thres 0.8 --gpu $gpu --attr_method $method --absolute
-python evaluate_attributions.py --prob_thres 0.8 --gpu $gpu --attr_method $method 
+python evaluate_attributions.py --results_path $results_dir --prob_thres 0.9 --gpu $gpu --attr_method $method --absolute 
+python evaluate_attributions.py --results_path $results_dir --prob_thres 0.9 --gpu $gpu --attr_method $method 
 ) 
