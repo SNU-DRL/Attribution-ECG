bcaae228db5cfb66bb5f877c84cfcd32b0ee3d42
Jangwon Suh <rxwe5607@snu.ac.kr>
2023-05-16 19:45:26 +0900

    update scripts for evaluating attribution methods

--- scripts/run_compute_attribution_mit-bih.sh
+++ scripts/run_compute_attribution_mit-bih.sh
@@ -18,7 +18,7 @@ N_SAMPLES=500
 # Settings
 GPU_NUM=0
 
-mkdir results_attr_230507
+mkdir results_attr_230516
 
 for ATTR_METHOD in random_baseline input_gradient saliency guided_backprop integrated_gradients deep_lift deep_shap lrp gradcam guided_gradcam lime kernel_shap
 do
@@ -27,7 +27,7 @@ do
         MODEL_DIR=$BASE_DIR'_seed'$SEED
         MODEL_PATH=results_230507/$MODEL_DIR/model_last.pt
 
-        RESULT_DIR=results_attr_230507/$MODEL_DIR/$ATTR_METHOD
+        RESULT_DIR=results_attr_230516/$MODEL_DIR/$ATTR_METHOD
         python compute_attribution.py \
             --dataset $DATASET \
             --dataset_path $DATASET_PATH \
