a8106505944428f5091af4171c495c19226bcb28
Jangwon Suh <rxwe5607@snu.ac.kr>
2024-04-07 12:41:23 +0900

    remove unnecessary files

--- scripts/others/plot_dist_histogram_ptbxl.sh
+++
@@ -1,6 +0,0 @@
-python plot_dist_histogram_ptbxl.py \
-    --dataset ptbxl \
-    --dataset_path ptb-xl/data/data_ptbxl/ptbxl_4leads.pkl \
-    --model_path results_ptbxl/results_training/ptbxl_4leads_resnet18_7_bs32_lr1e-4_wd1e-4_ep20_seed0/model_last.pt \
-    --gpu_num 2 \
-    --result_dir results_ptbxl/figures_plot_dist_histogram
